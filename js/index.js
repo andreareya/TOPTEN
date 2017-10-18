fd59b280e615cd801123c596e73e00f0da306e80
andreareya <rey.andrea@outlook.com>
2017-10-18 17:55:56 +0200

    Modification J3
    Changement de design :
    
    - Grille du "topten" à la place du carousel
    - ajout du lecteur spotify et modification de la taille

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 $('.search-button').click(function(){
   $(this).parent().toggleClass('open');
 });
+
+$.getJSON("https://api.airtable.com/v0/app3oxEZXINZHN8ux/musics?api_key=keydQHDgERxfAsnDe",
+                    function(data) {
+
+                     data_str = JSON.stringify(data);
+
+                     $('article').html(data_str);
+
+                    });
+
