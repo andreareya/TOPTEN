94b91693614cd896a8070b135f4b0322422af80a
andreareya <rey.andrea@outlook.com>
2017-10-20 14:11:43 +0200

    Commit après manger

--- js/index.js
+++ js/index.js
@@ -5,18 +5,15 @@ $('.search-button').click(function(){
 $(function(){
 	$(document).one('click', '.like-review', function(e) {
 		$(this).html('<i class="fa fa-heart" aria-hidden="true"></i> Vous aimez');
-		$(this).children('.fa-heart').addClass('animate-like');
 	});
 });
 
-$('.block').smoove({offset:'40%'});
-
-$.getJSON("https://api.airtable.com/v0/app3oxEZXINZHN8ux/musics?api_key=keydQHDgERxfAsnDe",
-                    function(data) {
-
-                     data_str = JSON.stringify(data);
-
-                     $('article').html(data_str);
-
-                    });
-
+$.getJSON("https://api.airtable.com/v0/app3oxEZXINZHN8ux/musics?api_key=key46IDre0cUesMxW",
+  					function(data) {
+              $.each(data.records, function(key,val) {
+                console.log(val.fields.title);
+                $('<li>'+val.fields.title+'</li>').appendTo("#musiques");
+                 $("<iframe src='https://open.spotify.com/embed?uri=" + val.fields['spotify-link'] + "'&view=coverart frameborder='0' allowtransparency='true'></iframe>").appendTo('#musiques');
+				      });
+  }
+);
